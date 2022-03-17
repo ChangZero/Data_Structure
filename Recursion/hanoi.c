313504910891d33a3c456244762a0edbc020188e
ChangZero <kimcy9973@gmail.com>
2022-03-17 19:11:29 +0900

    week01-2

---
+++ Recursion/hanoi.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+//A에서 C로 탑을 옮기는 문제!
+//n-1개 일때 A에서 B로 옮기는 것을 순환함수를 사용하여 호출 그 후 가장 마지막 탑을 C로 옮긴후 B에서 C로 옮기는 것을 순환함수로 다시 호출!  
+void hanoi_tower(int n, char from, char tmp, char to)
+{
+    if(n==1)
+        printf("원판 1을 %c에서 %c으로 옮긴다.\n", from, to);
+    else{
+        hanoi_tower(n-1,from, to, tmp);
+        printf("원판 %d을 %c에서 %c으로 옮긴다.\n", n, from, to);
+        hanoi_tower(n-1,tmp,from,to);
+    }
+}
+int main(void)
+{   
+    int num;
+    scanf("%d", &num);
+    hanoi_tower(num, 'A','B','C');
+}
---
+++ Recursion/power.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+double power(double x,int n)
+{
+    if(n==0)
+        return 1;
+    else if ((n%2)==0)
+        return power(x*x, n/2);
+    else
+        return x*power(x*x,(n-1)/2);
+}
+int main(void)
+{
+    double num;
+    int power_num;
+    scanf("%lf %d", &num, &power_num);
+    printf("%.2lf", power(num, power_num));
+    return 0;
+}
