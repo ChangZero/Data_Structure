b72632f86a3907aa1a0497456a0456fdce644092
ChangZero <kimcy9973@gmail.com>
2022-05-09 00:01:54 +0900

    week10-3

--- project3/proj3.c
+++ project3/proj3.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+typedef struct node *tree_ptr;
+typedef struct node{
+	int comp_num;
+	char comp_name[20];
+	int stock;
+	int recall;
+	tree_ptr left;
+	tree_ptr right;
+};
+
+tree_ptr ptr;
 
 int main()
 {
 	char cmd;
 	int comp_num;
-	char comp_naum[20];
+	char comp_name[20];
 	int stock;
 	int recall;
 	int add_num;
@@ -14,7 +25,7 @@ int main()
 
 	char str[100];
 
-	FILE *fp = fopen("input.txt", "r");
+	FILE *fp = fopen("C:\\Coding\\Cource\\Data Structure\\project3\\input.txt", "r");
 	if (fp == NULL)
 	{
 		printf("input file open error\n");
@@ -25,18 +36,36 @@ int main()
 
 	while (1)
 	{
-		fgets(str, 50, fp);
-		char *ptr = strtok(str, " ");
-		while (ptr != NULL)
+		fscanf(fp, "%c", &cmd);
+		switch (cmd)
 		{
-			switch (ptr)
-			{
-			case 'N':
-			}
-
-			ptr = strtok(NULL, " ");
+		case 'N':
+			fscanf(fp, "%d", &comp_num);
+			fgets(comp_name, 20, fp);
+			fscanf(fp, "%d %d", &stock, &recall);
+			printf("%d %s %d %d\n", comp_num, comp_name, stock, recall);
+			break;
+		case 'D':
+			fgets(comp_name, 20, fp);
+			printf("%s", comp_name);
+			break;
+		case 'A':
+			fscanf(fp, "%d %d", &comp_num, &add_num);
+			printf("%d %d\n", comp_num, add_num);
+			break;
+		case 'R':
+			fscanf(fp, "%d %d", &comp_num, &remove_num);
+			printf("%d %d\n", comp_num, remove_num);
+			break;
+		case 'P':
+			break;
+		case 'X':
+			exit(0);
+			break;
+		default:
+			break;
 		}
 	}
-
+	fclose(fp);
 	return 0;
 }
